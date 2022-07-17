ca98d42b65173a00f0b76cd5652980f2a706a5c9
hamid423 <74140209+hamid423@users.noreply.github.com>
2022-07-17 20:10:13 +0100

    source code button

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -12,7 +12,7 @@ const Download = () => {
           <h1 className={`${styles.h1Text} ${styles.blackText}`}>Download the source code</h1>
           <p className={`${styles.pText} ${styles.blackText}`}>Get the full source code on Github</p>
         </div>
-        <button className={styles.btnPrimary}>Source Code</button>
+        <button className={styles.btnPrimary}  onClick={() => window.open("https://github.com/hamid423/nftMarketplace_showcase.git", "_blank")}>Source Code</button>
         <div className={styles.flexCenter}>
           <img
             src={assets.mockup03}
